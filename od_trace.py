001787a2d95776c38a24f8fde3089f74ffde0bad
Liarod.v.RomanGol <romangoliard@gmail.com>
2013-01-13 16:27:00 +0800

    Version 0.003
    Fix bugs of od_trace.py
    
    add many tools include mem statistical analyais

--- od_trace.py
+++ od_trace.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*- 
-#中文支持
+# 中文支持
 
-import pylab
 import re
 
 ADDR_RE = '\[[A-Z0-9]{8}\]'
@@ -12,17 +11,6 @@ REG8_RE = '[ABCD][LH]'
 REG_TO_MEM = '\],'
 MEM_TO_REG = ',.*\['
 
-
-def draw_addr( addr_list ):
-    X = []
-    Y = []
-    counter = 0
-    for i in addr_list:
-        X.append(counter)
-        Y.append(i)
-        counter = counter + 1
-    pylab.plot( X, Y )
-    pylab.show() 
 
 def rewrite_opcode( opcode, m_dic, r_dic ):
     ops = opcode.split()
@@ -42,13 +30,12 @@ def rewrite_opcode( opcode, m_dic, r_dic ):
             #print ops[1]
             new_op = '%s=%s:%s|R'%(opnum[0], m_dic[1], m_dic[0])
             return new_op
-        return '%s=%s:%s'%(opnum[0], r_dic[1], opnum[1])
+        #return '%s=%s:%s'%(opnum[0], r_dic[1], opnum[1])
     if op == 'MOVZX':
         if re.search ( MEM_TO_REG, ops[1] ):
-            new_op = '%s=000000%s:%s|R'%(m_dic[0], m_dic[1], opnum[1])
+            new_op = '%s=%s:%s|R'%(opnum[0], m_dic[1], m_dic[0])
             return new_op
-        print ops[1]
-        return '%s=000000%s:%s'%(opnum[0], r_dic[1], opnum[1])
+        #return '%s=%08s:%s'%(opnum[0], r_dic[1], opnum[1])
     return opcode    
 
 # transform [xxxx]=xx or REG=xx as map [xxxx]:xx
@@ -75,36 +62,29 @@ def get_reg_map( inst ):
 
 if __name__=="__main__":
     f = open("data\\trace.txt")
+    g = open("data\\mil.txt", 'w')
+
     s_list = f.readlines()
     
-    addr_list = []
-    opcode_list = []
-
     addr_opcode_dic = {}
-    reg_dic = {}
-    mem_dic = {}
 
     for s in s_list:
         # record address and make addr-opcode map        
         inst = s.strip('\n').split('\t')
         addr = int(inst[0], 16)
-        addr_list.append( int(inst[0], 16) )
         addr_opcode_dic[addr] = inst[1]
 
         # translate opcode to mil
-
-        m = get_mem_map(s)
-        r = get_reg_map(s)
-        new_op = rewrite_opcode( inst[1], m, r )
-        opcode_list.append( new_op )
-    #draw_addr(addr_list)
-    #print addr_list
+        try:
+            new_op = rewrite_opcode( inst[1], get_mem_map(s), get_reg_map(s) )
+            g.write( inst[0] )
+            g.write( '||' )
+            g.write( new_op )
+            g.write('\n')
+        except:
+            print s
+            break
     #print addr_opcode_dic
-
-    g = open("data\\mil.txt", 'w')
-    for i in opcode_list:
-        g.write(i)
-        g.write('\n')
+    
     g.close()
-
     f.close()
--- rw.bat
+++ rw.bat
@@ -1,2 +1,3 @@
 findstr "|W" data\mil.txt > data\w.txt
 findstr "|R" data\mil.txt > data\r.txt
+findstr "|[RW]" data\mil.txt >> data\rw.txt
